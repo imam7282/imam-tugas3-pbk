14e56808430e048d097b5710589149743d78c519
Imam Palokoto <muhammadimamsamudra597@gmail.com>
2025-05-27 22:53:42 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import StudyCase from './components/StudyCase.vue'
+</script>
+
+<template>
+  <StudyCase />
+</template>
+
+<style scoped>
+/* Boleh hapus bagian style ini jika tidak perlu */
+</style>
---
+++ src/component/StudyCase.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+
+    <p :title="tooltip">Arahkan mouse ke teks ini</p>
+
+    <button @click="ubahPesan">Klik untuk ubah pesan</button>
+
+    <input v-model="inputText" placeholder="Tulis sesuatu..." />
+
+    <p v-if="inputText">Kamu menulis: {{ inputText }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Halo dari Vue!')
+const tooltip = 'Ini adalah tooltip'
+const inputText = ref('')
+
+function ubahPesan() {
+  message.value = 'Pesan telah diubah!'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
